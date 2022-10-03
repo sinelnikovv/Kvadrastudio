d488ebc08252add757b1b940a6a9f1aae248b84c
sinelnikovv <aktep010@gmail.com>
2022-10-04 00:17:00 +0300

    finish structure and styles

--- app.js
+++ app.js
@@ -80,3 +80,20 @@ var splide = new Splide('.splide', {
   gap: "20px",
 } );
 splide.mount();
+
+//animate letters
+
+const inViewport = (entries, observer) => {
+  entries.forEach(entry => {
+    entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+  });
+};
+
+const Obs = new IntersectionObserver(inViewport);
+const obsOptions = {};
+
+// Attach observer to every [data-inviewport] element:
+const ELs_inViewport = document.querySelectorAll('[data-inviewport]');
+ELs_inViewport.forEach(EL => {
+  Obs.observe(EL, obsOptions);
+});
